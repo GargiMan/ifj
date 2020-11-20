9eef6b71046a097d5c8dc0dd3d152ff01cbab282
Marek Gergel <mage26gg@gmail.com>
2020-11-20 22:54:28 +0100

    update/scanner
    now ignore comments (wont put in token list)

--- scanner.c
+++ scanner.c
@@ -7,21 +7,49 @@
 #include "scanner.h"
 
 void readFile() {
-    
-    int c = 0;
+
+    int c = 0, c2 = 0;
     String_t s = NULL;
     LListItem_t* temp = NULL;
 
     while (!IS_EOF(c)) {
 
-        c = getc(stdin);
+        if (c2) {
+            c = c2;
+            c2 = 0;
+        } else {
+            c = getchar();
+        }
 
-        if (!temp && !IS_NOT_TERM(c)) {
+        //slash -> comments or div
+        if (IS_SLASH(c)) {
+
+            c2 = getchar();
+
+            //signle line
+            if (IS_SLASH(c2)) {
+                while (!IS_EOL(c2)) c2 = getchar();
+                c2 = 0;
+                continue;
+            }
+
+            //multi line
+            if (IS_ASTERISK(c2)) {
+                while (!IS_EOF(c2 = getchar())) { 
+                    if (IS_ASTERISK(c2) && IS_SLASH(c2 = getchar())) break;
+                }
+                c2 = 0;
+                continue;
+            }
+        }
+
+        if (!temp && !IS_NOT_TERM(c)) { //need to add slash or destroy item
             ITEM_CREATE(temp);
             if (!temp) errorExit(internalError, "scanner.h : Item allocation failed");
             s = NULL;
         } 
 
+        //not terminals
         if (IS_NOT_TERM(c)) {
             if (!temp) continue;
             temp->token = s;
@@ -30,6 +58,7 @@ void readFile() {
             continue;
         }
 
+        //add char
         s = appendChar(s, c);
     } 
 
